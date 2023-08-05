61f3ba298ffe238da1d19a311cd1d62ee8443f05
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-06 00:57:49 +0600

    fetching data

---
+++ ajax.js
@@ -0,0 +1,5 @@
+// JavaScript code to fetch and print all data using fetch()
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error fetching data:', error));
